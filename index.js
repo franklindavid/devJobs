7b2026dda410effda7d9440cf15f800a1e640795
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2022-03-16 15:21:50 -0500

    Agregando view engine

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express= require('express');
+const exphbs = require('express-handlebars');
+const router = require('./routes');
+const path = require ('path');
 
 const app= express();
 
-app.use('/',(req,res)=>{
-    res.send('Funciona');
-});
+app.engine('handlebars',
+    exphbs({
+        defaultLayout: 'layout'
+    })
+);
+
+app.set('view engine','handlebars');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/',router());
 
 app.listen(5000);
