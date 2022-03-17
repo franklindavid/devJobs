a21f437169ecd8de90dd1972593565cd51a55cc2
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2022-03-16 19:55:18 -0500

    Configurando archivos y iniciando sesion con cookie-parser y  connect-mongo
    -Creando archivo de configuracion para acceso ala base de datos con el orm mongoose
    -Creando session con cookie-parser
    -Corregido nombre de paquete session->express-session
    -- cambio de sintaxis "connect-mongo" version v2 -> v4

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const mongoose = require ('mongoose');
+require('./config/db');
 const express= require('express');
 const {engine} = require('express-handlebars');
 const router = require('./routes');
+const cookieParser= require('cookie-parser');
+const session = require('express-session');
+const MongoStore= require('connect-mongo');
+// const MongoStore= require('connect-mongo')(session);
 const path = require ('path');
 require('dotenv').config({path: 'variables.env'});
 
@@ -15,6 +21,19 @@ app.engine('handlebars',
 app.set('view engine','handlebars');
 
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.key,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongoUrl: process.env.DATABASE,
+        mongooseConnection : mongoose.connection
+    })
+}));
 
 app.use('/',router());
 
