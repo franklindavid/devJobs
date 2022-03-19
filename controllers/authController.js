f4ad49303fc576c868747c5aaf946d26803425dc
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2022-03-19 11:25:48 -0500

    Creando panel de administracion

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,15 @@
 const passport = require('passport');
 
 exports.autenticarUsuario = passport.authenticate('local',{
-    succesRedirect :'/administracion',
+    successRedirect :'/administracion',
     failureRedirect : '/iniciar-sesion',
     failureFlash: true,
     badRequestMessage:'Ambos campos son obligatorios'
 })
+
+exports.mostrarPanel = (req,res) => {
+    res.render('administracion',{
+        nombrePagina: 'Panel de administracion',
+        tagline: 'Crea y Administra tus vacantes desde aqui'
+    });
+}
